0604ddf15f75084bfcd06c1778321b38fd3e77e2
Nikane Mallea <nikanemallea@nikane.local>
2019-12-13 07:16:37 +0100

    Game class methods
    * methods for starting game, getting phrase and handling interaction

--- js/Game.js
+++ js/Game.js
@@ -12,3 +12,36 @@ class Game {
     this.activePhrase = null;
   }
 }
+
+// class methods
+// hides the start screen overlay, calls the getRandomPhrase() method
+startGame() {
+  document.querySelector('#overlay').style.display = 'none';
+  this.activePhrase = this.getRandomPhrase();
+  this.activePhrase.addPhraseToDisplay();
+}
+// this method randomly retrieves one of the phrases stored in the phrases array and returns it
+getRandomPhrase() {
+  return this.phrases[Math.floor(Math.random() * this.phrases.length)];
+}
+// this method controls most of the game logic. It checks to see if the button clicked by the player matches a letter in the phrase, and then directs the game based on a correct or incorrect guess
+handleInteraction(button) {
+  button.disabled = true;
+  // checks if letter is in phrase
+  if (this.activePhrase.checkLetter(button.textContent)) {
+    // if true, show letters.
+    this.activePhrase.showMatchedLetter(button.textContent);
+    button.classList.add('chosen', 'animated', 'rotateIn');
+    // add animation to correct key pressed
+    if (this.checkForWin()) {
+      // if all letters guessed, Game Over.
+      this.gameOver()
+    }
+  } else {
+    // if false, remove life.
+    button.classList.add('wrong', 'animated', 'shake');
+    // add animation to wrong key pressed
+    this.removeLife();
+    this.checkForWin();
+  }
+}
